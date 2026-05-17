520c00748e8f31174dfee4252fe8eea6d350c562
Kit Ledru <kit@ledru.dev>
2026-05-17 14:41:44 +0200

    save

---
+++ dotfiles/dev/.oh-my-zsh/custom/i.zsh
@@ -0,0 +1,9 @@
+#    1iis infrastructure shell environment
+# --------------------------------------------
+# Uncomment these lines if you use 1iis/infra.
+
+# CLi to PATH
+#source /etc/profile.d/i.sh
+
+# env variables
+#source /i/cfg/realm.sh
